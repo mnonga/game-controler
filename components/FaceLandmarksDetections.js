cd1a5d3ce263c35e2ebcc1ffbe8bc7ccd0c0c782
Michee NONGA <nongamichee@gmail.com>
2025-03-07 12:27:25 +0100

    face and hand detector combined

--- components/FaceLandmarksDetections.js
+++ components/FaceLandmarksDetections.js
@@ -2,8 +2,9 @@ import { useEffect, useRef, useState } from 'react'
 import { useAnimationFrame } from '../lib/hooks/useAnimationFrame'
 import '@tensorflow/tfjs-backend-webgl'
 import * as tfjsWasm from '@tensorflow/tfjs-backend-wasm'
-import { drawFaces, drawNoses } from '../lib/utils'
+import { drawFaces, drawNoses, drawHands } from '../lib/utils'
 import * as faceLandmarksDetection from '@tensorflow-models/face-landmarks-detection'
+import * as handPoseDetection from '@tensorflow-models/hand-pose-detection'
 import * as faceMesh from '@mediapipe/face_mesh'
 import '@tensorflow-models/face-detection'
 import { LABEL_TO_COLOR } from '../lib/utils'
@@ -13,15 +14,17 @@ import {
   detectHeadTiltByShape,
   detectHeadTurn,
   detectHeadTurnByNose,
+  isLeftEyeOpen,
   isMouthOpen,
 } from '../lib/controls'
 
+// open https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm/
 //tfjsWasm.setWasmPaths(`https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm`);
 tfjsWasm.setWasmPaths(`/@tensorflow/tfjs-backend-wasm.js`)
 
 console.log(faceMesh.VERSION) // https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh@0.4.1633559619
 
-async function setupDetector() {
+async function setupDetectorFace() {
   const model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh
   const detector = await faceLandmarksDetection.createDetector(model, {
     runtime: 'mediapipe',
@@ -29,6 +32,19 @@ async function setupDetector() {
     solutionPath: `/@mediapipe/face_mesh`,
     maxFaces: 2,
     refineLandmarks: true,
+  })
+
+  return detector
+}
+
+async function setupDetectorHand() {
+  const model = handPoseDetection.SupportedModels.MediaPipeHands
+  const detector = await handPoseDetection.createDetector(model, {
+    runtime: 'mediapipe',
+    maxHands: 2,
+    // open https://cdn.jsdelivr.net/npm/@mediapipe/hands/, voir dans Network les trucs à download
+    //solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/hands'
+    solutionPath: '/@mediapipe/hands',
   })
 
   return detector
@@ -65,9 +81,11 @@ async function setupCanvas(video) {
 }
 
 export default function FaceLandmarksDetection({ onDirection, onMouthOpened, onTilt }) {
-  const detectorRef = useRef()
+  const detectorFaceRef = useRef()
+  const detectorHandRef = useRef()
   const videoRef = useRef()
   const [ctx, setCtx] = useState()
+
   const contours = faceLandmarksDetection.util.getKeypointIndexByContour(
     faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh
   )
@@ -75,15 +93,19 @@ export default function FaceLandmarksDetection({ onDirection, onMouthOpened, onT
   const [direction, setDirection] = useState(null)
   const [mouthOpened, setMouthOpened] = useState(false)
   const [tilt, setTilt] = useState(null)
+  const [leftEyeOpened, setLeftEyeOpened] = useState(false)
 
   useEffect(() => {
     async function initialize() {
       videoRef.current = await setupVideo()
       const ctx = await setupCanvas(videoRef.current)
-      detectorRef.current = await setupDetector()
+      detectorFaceRef.current = await setupDetectorFace()
+      detectorHandRef.current = await setupDetectorHand()
 
       setCtx(ctx)
     }
+
+    console.log({ contours })
 
     initialize()
   }, [])
@@ -102,20 +124,25 @@ export default function FaceLandmarksDetection({ onDirection, onMouthOpened, onT
 
   useAnimationFrame(
     async delta => {
-      const faces = await detectorRef.current.estimateFaces(videoRef.current)
+      const faces = await detectorFaceRef.current.estimateFaces(videoRef.current)
+      const hands = await detectorHandRef.current.estimateHands(videoRef.current, {
+        flipHorizontal: false,
+      })
 
       ctx.clearRect(0, 0, videoRef.current.videoWidth, videoRef.current.videoHeight)
       ctx.drawImage(videoRef.current, 0, 0)
       drawFaces(faces, ctx, contours)
       drawNoses(ctx, faces)
+      drawHands(hands, ctx)
 
       if (faces.length > 0) {
-        setDirection(detectHeadTurnByNose(faces[0]))
-        setMouthOpened(isMouthOpen(faces[0]))
-        setTilt(detectHeadTiltByNose(faces[0]))
+        //setDirection(detectHeadTurnByNose(faces[0]))
+        //setMouthOpened(isMouthOpen(faces[0]))
+        //setTilt(detectHeadTiltByNose(faces[0]))
+        //setLeftEyeOpened(isLeftEyeOpen(faces[0]))
       }
     },
-    !!(detectorRef.current && videoRef.current && ctx)
+    !!(detectorFaceRef.current && detectorHandRef.current && videoRef.current && ctx)
   )
 
   return (
@@ -129,6 +156,7 @@ export default function FaceLandmarksDetection({ onDirection, onMouthOpened, onT
         playsInline
         className="invisible scale-x-[-1] absolute top-0 left-0 w-0 h-0"
       ></video>
+      <div>letEye: {leftEyeOpened}</div>
     </>
   )
 }
